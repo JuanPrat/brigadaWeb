d70b35817695f61c2b84af3437aa51deea09e82a
Juan Jose Garcia Sosa <juan.garcia@sofka.com.co>
2020-12-12 17:45:40 -0500

    se desarrolla descarga de archivo excel con el estado de los kits de primeros auxilios

--- src/app/components/informes/informes.component.ts
+++ src/app/components/informes/informes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { kitPrimerosAuxilios } from 'src/app/models/implementos';
+import { ImplementosService } from 'src/app/services/implementos.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-informes',
@@ -6,13 +9,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informes.component.scss']
 })
 export class InformesComponent implements OnInit {
-  constructor() { }
+  kits: Array<kitPrimerosAuxilios> = new Array();
+
+
+  constructor(private implementosServ: ImplementosService) { }
 
   ngOnInit(): void {
   }
 
-  descargarInforme(){
-  
+  descargarInforme() {
+    this.implementosServ.obtenerKits().then(metadata => metadata.docs.forEach(element => {
+      console.log(element)
+      debugger
+      this.kits.push({
+        algodon: this.transformValueIntoBussiness(element.data().algodon),
+        curas: this.transformValueIntoBussiness(element.data().curas),
+        fosforos: this.transformValueIntoBussiness(element.data().fosforos),
+        gasa: this.transformValueIntoBussiness(element.data().gasa),
+        isodine: this.transformValueIntoBussiness(element.data().isodine),
+        linterna: this.transformValueIntoBussiness(element.data().linterna),
+        microporo: this.transformValueIntoBussiness(element.data().microporo),
+        paletasMadera: this.transformValueIntoBussiness(element.data().paletasMadera),
+        solucionSalina: this.transformValueIntoBussiness(element.data().solucionSalina),
+        tijeras: this.transformValueIntoBussiness(element.data().tijeras),
+        nombre: element.data().nombre,
+        disponible: this.transformValueIntoBussiness(element.data().disponible).toString(),
+        brigadista: element.data().brigadista
+      })
+    })
+    ).finally(() => {
+      /* generate a worksheet */
+      debugger
+      var ws = XLSX.utils.json_to_sheet(this.kits);
+
+      /* add to workbook */
+      var wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, "Kits");
+
+      /* write workbook and force a download */
+      XLSX.writeFile(wb, "EstadoKits.xlsx");
+    })
   }
 
+  transformValueIntoBussiness(value: string | boolean): string {
+    debugger
+    switch (value) {
+      case ("1"):
+        return "Bajo";
+        break;
+      case ("2"):
+        return "Medio";
+        break;
+      case ("3"):
+        return "Alto";
+        break;
+      case ("true"):
+        return "Disponible"
+        break;
+      case (true):
+        return "Disponible"
+        break;
+      default:
+        return value.toString();
+        break;
+    }
+  }
 }
--- src/app/models/implementos.ts
+++ src/app/models/implementos.ts
@@ -1,27 +1,27 @@
 export interface kitPrimerosAuxilios {
     nombre:string;
-    algodon: 0;
-    curas: 0;
-    disponible: true;
-    fosforos: 0;
-    gasa: 0;
-    isodine: 0;
-    linterna: true;
-    microporo: 0; 
-    paletasMadera: 0;
-    solucionSalina: 0;
-    tijeras: true;
-    brigadista: String;
+    algodon: string;
+    curas: string;
+    disponible: string | boolean;
+    fosforos: string;
+    gasa: string;
+    isodine: string;
+    linterna: string | boolean;
+    microporo: string; 
+    paletasMadera: string;
+    solucionSalina: string;
+    tijeras: string | boolean;
+    brigadista: string;
 }
 
 export interface audifonos {
     nombre:string;
     disponible: false;
-    brigadista: String;
+    brigadista: string;
 }
 
 export interface radio{
     nombre:string;
     disponible: false;
-    brigadista: String;
+    brigadista: string;
 }
